6d5521eec65218fb88a9b4cda18f2c727c4c836b
hvrbferrer <hvrbferrer@gmail.com>
2018-10-09 22:55:16 +0100

    add read method

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,6 @@
 public abstract class Model {
     abstract void create() throws CustomException;
+    abstract void read() throws CustomException;
     abstract void update() throws CustomException;
     abstract void validator() throws CustomException;
     abstract void delete() throws CustomException;
