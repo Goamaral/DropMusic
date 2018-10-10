775fdc2bed51dd46c31ea0269139769280359256
Goncalo Amaral <goa.docs@gmail.com>
2018-10-11 00:32:13 +0100

    Refactor and gitignore update

--- Server/src/UserInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface UserInterface extends Remote {
-    // Controller
-    void login(User user) throws RemoteException, CustomException;
-    void register(User user) throws RemoteException, CustomException;
-}
